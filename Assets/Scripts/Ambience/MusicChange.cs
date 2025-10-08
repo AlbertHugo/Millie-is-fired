85e3994d5f8b8bd5308cc4bd3c3841fd2361f1a1
Epic-Artist25 <epic.artist25@gmail.com>
2025-10-08 18:11:16 -0300

    Commit no código alternativo de chance em Game Over

---
+++ Assets/Scripts/Ambience/MusicChange.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MusicChange : MonoBehaviour
+{
+    [Header("Efeitos sonoros")]
+    private int entranceIndex = 0;
+    public AudioClip entrance1;
+    public AudioClip entrance2;
+
+    void Start()
+    {
+        entranceIndex = Random.Range(0, 3);//seleciona qual o música que será tocada na entrada
+        //toca o som na entrada, ou não toca nada
+        if (entranceIndex <= 1)
+        {
+            PlaySound(entrance1);
+        }
+        else if(entranceIndex == 0)
+        {
+            PlaySound(entrance2);
+        }
+    }
+     public static void PlaySound(AudioClip clip)
+    {
+        GameObject tempGO = new GameObject("TempAudio");
+        AudioSource aSource = tempGO.AddComponent<AudioSource>();
+        aSource.clip = clip;
+        aSource.Play();
+        GameObject.Destroy(tempGO, clip.length);
+    }
+}
