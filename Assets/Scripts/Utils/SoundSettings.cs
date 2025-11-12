0ac1bedf83260616cbe6663567d376fef4659320
XavierPUC <164264440+XavierPUC@users.noreply.github.com>
2025-11-12 12:55:47 -0300

    Alterações de audio
    Sliders adicionados para as configurações, sistema de SoundSettings atualizado para funcionar de acordo.

--- Assets/Scripts/Utils/SoundSettings.cs
+++ Assets/Scripts/Utils/SoundSettings.cs
@@ -4,31 +4,58 @@ using UnityEngine.UI;
 
 public class SoundSettings : MonoBehaviour
 {
-    [SerializeField] Slider soundSlider;
-    [SerializeField] AudioMixer masterMixer;
+    [Header("Sliders")]
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    [Header("Audio Mixer")]
+    [SerializeField] private AudioMixer masterMixer;
 
     private void Start()
     {
-        SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100));
+        // Carrega valores salvos (ou 100 se não houver)
+        float masterVol = PlayerPrefs.GetFloat("SavedMasterVolume", 100);
+        float musicVol = PlayerPrefs.GetFloat("SavedMusicVolume", 100);
+        float sfxVol = PlayerPrefs.GetFloat("SavedSFXVolume", 100);
+
+        // Atualiza cada volume
+        SetVolume("Master", masterVol);
+        SetVolume("Music", musicVol);
+        SetVolume("SFX", sfxVol);
     }
 
-    public void SetVolume(float _value)
+  
+    public void SetVolume(string target, float value)
     {
-        if (_value < 1)
-            _value = .001f;
+        if (value < 1)
+            value = 0.001f; 
 
-        RefreshSlider(_value);
-        PlayerPrefs.SetFloat("SavedMasterVolume", _value);
-        masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);
+
+        RefreshSlider(target, value);
+
+        PlayerPrefs.SetFloat($"Saved{target}Volume", value);
+
+        masterMixer.SetFloat($"{target}Volume", Mathf.Log10(value / 100f) * 20f);
     }
 
-    public void SetVolumeFromSlider()
+    public void SetVolumeFromSlider(string target)
     {
-        SetVolume(soundSlider.value);
+        switch (target)
+        {
+            case "Master": SetVolume("Master", masterSlider.value); break;
+            case "Music": SetVolume("Music", musicSlider.value); break;
+            case "SFX": SetVolume("SFX", sfxSlider.value); break;
+        }
     }
 
-    public void RefreshSlider(float _value)
+    private void RefreshSlider(string target, float value)
     {
-        soundSlider.value = _value;
+        switch (target)
+        {
+            case "Master": masterSlider.value = value; break;
+            case "Music": musicSlider.value = value; break;
+            case "SFX": sfxSlider.value = value; break;
+        }
     }
 }
