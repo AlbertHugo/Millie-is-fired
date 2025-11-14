24221a2d75f00d81f9e28797e2efcbb8bab14ac2
XavierPUC <164264440+XavierPUC@users.noreply.github.com>
2025-11-14 13:50:36 -0300

    Mudanças no menu para build e no jogo
    Ainda não completamente funcional, mas estamos quase lá, falta pouco.

--- Assets/Scripts/HUD/BuildSettings.cs
+++ Assets/Scripts/HUD/BuildSettings.cs
@@ -1,12 +1,11 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class BuildSettings : MonoBehaviour
 {
     public GameObject player;
     private PlayerStats playerStats;
 
+    public GameObject buildPanel;
 
     bool choseDamage = false;
     bool choseHP = false;
@@ -23,103 +22,129 @@ public class BuildSettings : MonoBehaviour
     public GameObject ruler;
     public GameObject stapler;
 
+    [Header("UI Buttons")]
+    public GameObject btnDamage;
+    public GameObject btnHP;
+    public GameObject btnSpeed;
+
+    public GameObject btnRuler;
+    public GameObject btnStapler;
+    public GameObject btnPaper;
+
+    public GameObject btnPen;
+
     private void Start()
     {
-        playerStats = player.GetComponent<PlayerStats>();
 
-        if (choseDamage == true)
+        choseDamage = PlayerPrefs.GetInt("choseDamage", 0) == 1;
+        choseHP = PlayerPrefs.GetInt("choseHP", 0) == 1;
+        choseSpeed = PlayerPrefs.GetInt("choseSpeed", 0) == 1;
+
+        chosePaper = PlayerPrefs.GetInt("chosePaper", 0) == 1;
+        choseStapler = PlayerPrefs.GetInt("choseStapler", 0) == 1;
+        choseRuler = PlayerPrefs.GetInt("choseRuler", 0) == 1;
+
+        chosePen = PlayerPrefs.GetInt("chosePen", 0) == 1;
+
+        if (player != null)
         {
-            playerStats.ATKBuff += 0.1f;
+            playerStats = player.GetComponent<PlayerStats>();
+
+            if (choseDamage) playerStats.ATKBuff += 0.1f;
+            if (choseHP) playerStats.HPBuff += 1f;
+            if (choseSpeed) playerStats.SPDBuff += 0.1f;
+
+            if (chosePaper)
+            {
+                PlayerAttack.projectile = paper;
+                PlayerAttack.projectileIndex = 2;
+            }
+
+            if (choseStapler)
+            {
+                PlayerAttack.projectile = stapler;
+                PlayerAttack.projectileIndex = 1;
+            }
+
+            if (choseRuler)
+            {
+                PlayerAttack.projectile = ruler;
+                PlayerAttack.projectileIndex = 0;
+            }
+
+            if (chosePen)
+                Pen.SetActive(true);
         }
 
-        if (choseHP == true)
-        {
-            playerStats.HPBuff += 1f;
-        }
-
-        if (choseSpeed == true)
-        {
-            playerStats.SPDBuff += 0.1f;
-        }
-
-        if (chosePaper == true)
-        {
-            PlayerAttack.projectile = paper;
-            PlayerAttack.projectileIndex = 2;
-
-        }
-
-        if (choseStapler == true)
-        {
-            PlayerAttack.projectile = stapler;
-            PlayerAttack.projectileIndex = 1;
-        }
-
-        if (choseRuler == true)
-        {
-            PlayerAttack.projectile = ruler;
-            PlayerAttack.projectileIndex = 0;
-        }
-
-        if (chosePen == true)
-        {
-            Pen.SetActive(true);
-        }
+        RefreshButtons();
     }
-  
-    void Update()
+
+
+
+    public void BuildDamage()
     {
-        
+        choseDamage = true;
+        PlayerPrefs.SetInt("choseDamage", 1);
+        RefreshButtons();
     }
 
-
-    void BuildDamage()
-    {
-       choseDamage = true;
-    }
-
-    void BuildHP() 
+    public void BuildHP()
     {
         choseHP = true;
+        PlayerPrefs.SetInt("choseHP", 1);
+        RefreshButtons();
     }
 
-    void BuildSpeed()
+    public void BuildSpeed()
     {
         choseSpeed = true;
+        PlayerPrefs.SetInt("choseSpeed", 1);
+        RefreshButtons();
     }
 
-    void BuildRuler()
+    public void BuildRuler()
     {
         choseRuler = true;
+        PlayerPrefs.SetInt("choseRuler", 1);
+        RefreshButtons();
     }
 
-    void BuildStapler()
+    public void BuildStapler()
     {
         choseStapler = true;
+        PlayerPrefs.SetInt("choseStapler", 1);
+        RefreshButtons();
     }
 
-    void BuildPaper()
+    public void BuildPaper()
     {
         chosePaper = true;
+        PlayerPrefs.SetInt("chosePaper", 1);
+        RefreshButtons();
     }
 
-    void BuildPen()
+    public void BuildPen()
     {
-        PlayerUltimate.haveUlt=true;
-        PlayerUltimate.ultIndex=1;
+        chosePen = true;
+        PlayerPrefs.SetInt("chosePen", 1);
+        PlayerUltimate.haveUlt = true;
+        PlayerUltimate.ultIndex = 1;
+        RefreshButtons();
     }
 
 
     public void RefundBuild()
     {
-        playerStats.HPBuff = 0;
-        playerStats.SPDBuff = 1f;
-        playerStats.ATKBuff = 1f;
+        if (player != null)
+        {
+            playerStats.HPBuff = 0;
+            playerStats.SPDBuff = 1f;
+            playerStats.ATKBuff = 1f;
+        }
 
         PlayerAttack.projectile = null;
 
-        PlayerUltimate.haveUlt=false;;
-        PlayerUltimate.ultIndex=0;
+        chosePen = false;
         choseDamage = false;
         choseHP = false;
         choseSpeed = false;
@@ -127,5 +152,45 @@ public class BuildSettings : MonoBehaviour
         chosePaper = false;
         choseStapler = false;
         choseRuler = false;
+
+        PlayerPrefs.DeleteKey("choseDamage");
+        PlayerPrefs.DeleteKey("choseHP");
+        PlayerPrefs.DeleteKey("choseSpeed");
+
+        PlayerPrefs.DeleteKey("chosePaper");
+        PlayerPrefs.DeleteKey("choseStapler");
+        PlayerPrefs.DeleteKey("choseRuler");
+
+        PlayerPrefs.DeleteKey("chosePen");
+
+        PlayerPrefs.Save();
+
+        RefreshButtons();
+    }
+
+    void RefreshButtons()
+    {
+
+        bool anyUpgradeChosen = choseDamage || choseHP || choseSpeed;
+
+        btnDamage.SetActive(!anyUpgradeChosen || choseDamage);
+        btnHP.SetActive(!anyUpgradeChosen || choseHP);
+        btnSpeed.SetActive(!anyUpgradeChosen || choseSpeed);
+
+
+        bool anyWeaponChosen = choseRuler || choseStapler || chosePaper;
+
+        btnRuler.SetActive(!anyWeaponChosen || choseRuler);
+        btnStapler.SetActive(!anyWeaponChosen || choseStapler);
+        btnPaper.SetActive(!anyWeaponChosen || chosePaper);
+
+
+        PlayerUltimate.haveUlt = false; 
+        PlayerUltimate.ultIndex = 0;
+    }
+
+    public void HideBuildPanel()
+    {
+        buildPanel.SetActive(false);
     }
 }
